7d0f6f847501c55febe02c885c13d900c7c0a535
yihangHKU <yhangli@connect.hku.hk>
2021-05-06 22:52:25 +0800

    move back vision_detect

--- src/vision_detect.hpp
+++ src/vision_detect.hpp
@@ -34,22 +34,6 @@ vector<float> RDF_2_FRD(vector<float> vector)
     return vector;
 }
 
-Eigen::Matrix<float, 3, 1> ENU_2_NED(Eigen::Matrix<float, 3, 1> vector)
-{
-    float buffer = vector[0];
-    vector[0] = vector[1];
-    vector[1] = buffer;
-    vector[2] = -vector[2];
-    return vector;
-}
-
-Eigen::Matrix<float, 3, 1> FLU_2_FRD(Eigen::Matrix<float, 3, 1> vector)
-{
-	vector[1] = - vector[1];
-	vector[2] = - vector[2];
-	return vector;
-}
-
 vector<float> vector_normalize(vector<float> vector)
 {   
     float length = sqrt(pow(vector[0], 2) + pow(vector[1], 2) + pow(vector[2], 2));
@@ -64,73 +48,47 @@ float parallel_check(float vector1[3], float vector2[3])
     return abs(vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector1[2]);
 }
 
-void plane_from_points(vector<Point3f> points, vector<float> &direction_, vector<float> &centroid_)
-{   
-    vector<float> sum = {0.0, 0.0, 0.0};
-    for (int i = 0; i < points.size(); i++)
-    {   
-        sum[0] += points[i].x;
-        sum[1] += points[i].y;
-        sum[2] += points[i].z;
-    }
-    for (int i = 0; i < 3; i++)
+float get_depth_scale(rs2::device dev)
+{
+    // Go over the device's sensors
+    for (rs2::sensor& sensor : dev.query_sensors())
     {
-        centroid_[i] = sum[i] * (1.0 / int(points.size()));
+        // Check if the sensor if a depth sensor
+        if (rs2::depth_sensor dpt = sensor.as<rs2::depth_sensor>())
+        {
+            return dpt.get_depth_scale();
+        }
     }
-    float xx = 0.0;
-    float xy = 0.0;
-    float xz = 0.0;
-    float yy = 0.0;
-    float yz = 0.0;
-    float zz = 0.0;
-    for (int i =0; i < points.size(); i++)
+    throw std::runtime_error("Device does not have a depth sensor");
+}
+
+void remove_background(rs2::video_frame& other_frame, const rs2::depth_frame& depth_frame, float depth_scale, float clipping_dist)
+{
+    const uint16_t* p_depth_frame = reinterpret_cast<const uint16_t*>(depth_frame.get_data());
+    uint8_t* p_other_frame = reinterpret_cast<uint8_t*>(const_cast<void*>(other_frame.get_data()));
+
+    int width = other_frame.get_width();
+    int height = other_frame.get_height();
+    int other_bpp = other_frame.get_bytes_per_pixel();
+
+    #pragma omp parallel for schedule(dynamic) //Using OpenMP to try to parallelise the loop
+    for (int y = 0; y < height; y++)
     {
-        vector<float> r = {points[i].x - centroid_[0], points[i].y - centroid_[1], points[i].z - centroid_[2]};
-        xx += r[0] * r[0];
-        xy += r[0] * r[1];
-        xz += r[0] * r[2];
-        yy += r[1] * r[1];
-        yz += r[1] * r[2];
-        zz += r[2] * r[2];
+        auto depth_pixel_index = y * width;
+        for (int x = 0; x < width; x++, ++depth_pixel_index)
+        {
+            // Get the depth value of the current pixel
+            auto pixels_distance = depth_scale * p_depth_frame[depth_pixel_index];
+
+            // Check if the depth value is invalid (<=0) or greater than the threashold
+            if (pixels_distance <= 0.f || pixels_distance > clipping_dist)
+            {
+                // Calculate the offset in other frame's buffer to current pixel
+                auto offset = depth_pixel_index * other_bpp;
+
+                // Set pixel to "background" color (0x999999)
+                std::memset(&p_other_frame[offset], 0x99, other_bpp);
+            }
+        }
     }
-    
-    float det_x = yy * zz - yz * yz;
-    float det_y = xx * zz - xz * xz;
-    float det_z = xx * yy - xy * xy;
-    float det_max = max(max(det_x, det_y), det_z);
-    vector<float> direction;
-    if (det_max == det_x)
-    {
-        direction_[0] = det_x;
-        direction_[1] = xz * yz - xy * zz;
-        direction_[2] = xy * yz - xz * yy;
-    }
-    else if (det_max == det_y)
-    {
-        direction_[0] = xz * yz - xy * zz;
-        direction_[1] = det_y;
-        direction_[2] = xy * xz - yz * xx;
-    }
-    else
-    {
-        direction_[0] = xy * yz - xz * yy;
-        direction_[1] = xy * xz - yz * xx;
-        direction_[2] = det_z;
-    }
-    float length = norm(direction_);
-    direction_[0] = direction_[0] / length;
-    direction_[1] = direction_[1] / length;
-    direction_[2] = direction_[2] / length;
-    // cout << "plane centroid: ";
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     cout << " " << centroid_[i];
-    // }
-    // cout << endl;
-    // cout << "plane norm direction: ";
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     cout << " " << direction_[i];
-    // }
-    // cout << endl;
 }
